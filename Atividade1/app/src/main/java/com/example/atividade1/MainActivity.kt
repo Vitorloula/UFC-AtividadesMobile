1be927788ec05523072ac6ee536842b30ac2c2fc
Vitor <vitorloula3@gmail.com>
2024-11-18 19:41:40 -0300

    Atividade 1

--- Atividade1/app/src/main/java/com/example/atividade1/MainActivity.kt
+++ Atividade1/app/src/main/java/com/example/atividade1/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.atividade1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.d("Lifecycle", "onCreate() chamado")
+        Toast.makeText(this, "onCreate() chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "onStart() chamado")
+        Toast.makeText(this, "onStart() chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Lifecycle", "onResume() chamado")
+        Toast.makeText(this, "onResume() chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Lifecycle", "onPause() chamado")
+        Toast.makeText(this, "onPause() chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Lifecycle", "onStop() chamado")
+        Toast.makeText(this, "onStop() chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Lifecycle", "onRestart() chamado")
+        Toast.makeText(this, "onRestart() chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Lifecycle", "onDestroy() chamado")
+        Toast.makeText(this, "onDestroy() chamado", Toast.LENGTH_SHORT).show()
     }
 }
